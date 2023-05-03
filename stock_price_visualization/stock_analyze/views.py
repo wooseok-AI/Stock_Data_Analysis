009279fb08fe609fc5172e35c86d67bcb0b46cab
wooseok-AI <peterjung95@gmail.com>
2023-05-03 18:31:30 +0900

    Add Stock_infro function

--- stock_price_visualization/stock_analyze/views.py
+++ stock_price_visualization/stock_analyze/views.py
@@ -5,21 +5,41 @@ from datetime import datetime
 from .models import Stock, Price
 
 import pandas as pd
+import matplotlib.pyplot as plt
 import csv
 
 from .utils import get_chart
 
+plt.switch_backend("AGG")
 
 def index(request):
 
-    # 모델 데이터 가져오기
-    stock_data = Stock.objects.all()
-    price_data = Price.objects.all()
+    return render(request, "stock_analyze/index.html")    
 
-    price_data_df = pd.DataFrame(price_data.values())[["date", "close_price"]]
-    price_data_df["date"] = price_data_df["date"].apply(lambda x : pd.to_datetime(str(x).split()[0]))
-    
-    chart = get_chart(price_data_df)
+def stock_info(request):
+    if request.method == "GET":
+        stock_name = request.GET.get("name")
+        start_date = request.GET.get("start_date")
+        end_date = request.GET.get("end_date")
+        print(stock_name, start_date, end_date)
+        stock_data = Stock.objects.all()
+        print("Request")
+        price_data = Price.objects.filter(date__range=[start_date, end_date])
+        if price_data.exists():
+                price_data_df = pd.DataFrame(price_data.values())[["date", "close_price"]]
+                price_data_df["date"] = price_data_df["date"].apply(lambda x : pd.to_datetime(str(x).split()[0]))
+                price_data_df = price_data_df.groupby("date")["close_price"].last().reset_index()
+                print(price_data_df.head())
+                fig = plt.figure(figsize=(10, 4))
+                chart = get_chart(price_data_df, fig=fig)
 
-    return render(request, "stock_analyze/index.html", {"stock_data": stock_data, "price_data": price_data, "chart":chart})    
-
+                context = {'stock_data': stock_data, 'price_data' : price_data_df.to_html(), 'chart' : chart}
+                return render(request, "stock_analyze/index.html", context)
+        else:
+            error_message = 'There is no stock data for the specified period.'
+            context = {'error_message': error_message}
+            return render(request, 'stock_analyze/index.html', context)
+    else:
+        error_message = 'Please enter stock name and date range.'
+        context = {'error_message': error_message}
+        return render(request, 'stock_analyze/index.html', context)
